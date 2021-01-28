ff31425443d0390f77c31ffc039f64c685ec6dd5
Jabair Khan <jabairk@gmail.com>
2021-01-28 04:07:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(scale, value_to_find)
   counter = 0 
   while counter < scale.length do
   if scale[counter] == value_to_find
- return scale[counter]
+ return counter
 #binding.pry
     end
 counter += 1
